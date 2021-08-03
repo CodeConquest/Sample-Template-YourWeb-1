3d570e871927044597f4074304040701ab0f4e30
Code_Conquest <88194473+CodeConquest@users.noreply.github.com>
2021-08-04 03:32:39 +0800

    Add files via upload

---
+++ YourWebTemplate1/footer.php
@@ -0,0 +1,17 @@
+<div class="card text-center">
+  <div >
+    Template By
+  </div>
+  <div class="card-body">
+    <h5 class="card-title">Relox Jhester Cris V</h5>
+    <p class="card-text">Team YourWeb 2021 copyright©</p>
+    <p class="card-text">My Social Links</p>
+  </div>
+
+  <div class=" text-muted">
+   <a class="fab fa-github" href="https://github.com/CodeConquest" style="color: black;"></a>
+   <a class="fab fa-facebook" href="https://www.facebook.com/profile.php?id=100070428750637" style="color: rgb(46, 172, 223);"></a>
+   <a  class="fab fa-instagram-square" href="#" style="color: rgb(233, 51, 81);"></a>
+  
+  </div>
+</div>
---
+++ YourWebTemplate1/index.php
@@ -0,0 +1,75 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css.map">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <script defer src="js/bootstrap.js"></script>
+    <script src="https://kit.fontawesome.com/acdc6e8cb8.js" crossorigin="anonymous"></script>
+    <title>Yourweb</title>
+  </head>
+  <body>
+    
+    
+      <!--Section Header-->
+
+     <?php include ('header.php') ?>
+
+ <!--End header-->
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+ <!--Section first heading-->
+
+ <div class="jumbotron jumbotron-fluid">
+  <div class="container">
+    <h1 class="kingina">Hello Clients!</h1>
+    <p class="lead">Do you require a website for your company? Thank you for visiting our website. YourWeb is a web marketing company that will assist you in your business by creating a website based on what you want in your business!</p>
+    <p class="lead">
+      <a class="btn btn-primary btn-lg" href="#" role="button">Get Started?</a>
+    </p>
+  </div>
+</div>
+
+ <!--End-->
+
+ 
+
+
+<!-- Footer -->
+
+
+<?php include ('footer.php') ?>
+<!--End Footer-->
+
+
+
+
+
+
+
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+      <script defer src="js/bootstrap.min.js"></script>
+      <script defer src="js/bootstrap.bundle.js"></script>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </body>
+</html>
